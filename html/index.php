c3b83d4465ba9c88b3dbe61a02531e4f34c2662c
oisiimutou <sorodoraper.01@gmail.com>
2019-08-06 17:05:11 +0900

    ページを２分割した

--- html/index.php
+++ html/index.php
@@ -13,46 +13,50 @@
     <header>
         <h1>正木翔也のホームページ</h1>
     </header>
-    <section id="profile"><img src="/images/profile.jpg" alt="自画像" class="width-200">
-        <ul>
-            <ul>
-                <li>正木翔也　（まさきしょうや）
-                </li>
-                <li>生年月日（１９９６年９月１６日）</li>
-                <li>血液型O型</li>
-                <li><a href="#">プロフィール</a></li>
-            </ul>
-        </ul>
-    </section id="address">
-    <section class="width-200"><span>If you have any enquiries regarding </span>
-        <div>mail:shouya masaki@gmail.com</div>
-        <div> <a href="mailto:shouya masaki@gmail.com">shouya masaki@gmail.com</a></div>
-    </section>
-    <section id="history">
-        <div>所属:
+    <div id="content">
+        <div id="content-left">
+            <section id="profile"><img src="/images/profile.jpg" alt="自画像" class="width-200">
+                <ul>
+                    <li>正木翔也　（まさきしょうや）</li>
+                    <li>生年月日（１９９６年９月１６日</li>
+                    <li>血液型O型</li>
+                    <li><a href="#">プロフィール</a></li>
+                </ul>
+            </section>
+            <section id="address" class="width-200">
+                <span>If you have any enquiries regarding </span>
+                <div><a href="mailto:shouya masaki@gmail.com">shouya masaki@gmail.com</a></div>
+            </section>
+            <section id="address">
+                <div>所属:</div>
+                <div>オフィスエンスペース</div>
+                <div>980-9999</div>
+                <div>宮城県仙台市青葉区国分寺1-1-1</div>
+                <div>TEL: 022-1234-5678</div>
+                <div>FAX: 022-1234-5678</div>
+            </section>
         </div>
-        <div>オフィスエンスペース</div>
-        <div>980-9999</div>
-        <div>宮城県仙台市青葉区国分寺1-1-1</div>
-        <div>TEL: 022-1234-5678</div>
-        <div>FAX: 022-1234-5678</div>
-    </section>
-    <section>
-        <h3>★★★　更新情報　★★★</h3>
-        <article>
-            <h4>舞台</h4>
-            <div>そろそろ暑くなたよ</div>
-        </article>
-        <article>
-            <h4>TV
-            </h4>
-            <div>おもしろ番組ないかな</div>
-        </article>
-        <article>
-            <h4>映画</h4>
-            <div>映画おもしろかった。阿部（仮）</div>
-        </article>
-    </section>
+        <div id="content-right">
+            <section id="history">
+                <h3>★★★　更新情報　★★★</h3>
+                <article>
+                    <h4>舞台</h4>
+                    <div>そろそろ暑くなたよ</div>
+                </article>
+                <article>
+                    <h4>TV
+                    </h4>
+                    <div>おもしろ番組ないかな</div>
+                </article>
+                <article>
+                    <h4>映画</h4>
+                    <div>映画おもしろかった。阿部（仮）</div>
+                </article>
+            </section>
+
+        </div>
+
+    </div>
 
 
 
