8e5541ad1b2cdf8e88db608382e1dfa381434797
oisiimutou <sorodoraper.01@gmail.com>
2019-08-06 13:57:14 +0900

    first commit

---
+++ html/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>初めてのphp</title>
+</head>
+<body>
+    
+</body>
+<h1>
+初めてのphp
+</h1>
+<h2>ヘッダー</h2>
+<div>divタブ</div>
+<div>小文字</div>
+</html>
